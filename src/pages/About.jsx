38b3bb47ad7184f001344fc22d0f6e9f9e26fe33
Carah Tychewicz <10439123+carah-tychewicz@users.noreply.github.com>
2024-07-02 23:39:09 -0500

    added bio

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,17 @@
+import "../styles/About.css";
+
 function About() {
   return (
     <>
-      <p>This could be an About page</p>
+      <div className="container">
+        <div>
+          <p>
+            This site, I have nothing to use it for, 
+            and I can't write an artist's statement or summarize my self or my life. I'm afraid. 
+            Can't it just be a wall of my drawings? I have no voice, just a right hand to draw with.
+          </p>
+        </div>
+      </div>
     </>
   )
 }
