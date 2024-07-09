500e5c9a2e8e9d2a101994eacdf5e837f9e6fb9e
Carah Tychewicz <10439123+carah-tychewicz@users.noreply.github.com>
2024-07-09 14:44:37 -0500

    home bones

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import About from './pages/About.jsx'
-import Contact from './pages/Contact.jsx'
-import Gallery from './pages/Gallery.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import About from './pages/About.jsx';
+import Contact from './pages/Contact.jsx';
+import Gallery from './pages/Gallery.jsx';
+import Home from './pages/Home.jsx';
+import './index.css';
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-import Navbar from './components/Navbar.jsx'
+import Navbar from './components/Navbar.jsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: <Home />,
   },
   {
     path: "/about",
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import '../styles/Home.css';
+
+function Home() {
+  return (
+    <>
+      <p>Hewwo Home.</p>
+    </>
+  )
+}
+
+export default Home;
