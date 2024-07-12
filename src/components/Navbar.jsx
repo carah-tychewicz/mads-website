65d0d12015c621793df8c68c54cd8bf18f584fbc
Carah Tychewicz <10439123+carah-tychewicz@users.noreply.github.com>
2024-07-12 14:56:48 -0500

    arranged navbar items in a more pleasing manner. renamed Home anchor tage to MADS, will be wordmark eventually.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,10 +4,10 @@ function Navbar() {
   return (
     <>
       <div className="nav-box">
-        <a className="nav-item" href="/">Home</a>
-        <a className="nav-item" href="/portfolio">Portfolio</a>
-        <a className="nav-item" href="/about">About</a>
+        <a className="name-logo" href="/">MADS</a>
         <a className="nav-item" href="/contact">Contact</a>
+        <a className="nav-item" href="/about">About</a>
+        <a className="nav-item" href="/portfolio">Portfolio</a>
       </div>
     </>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,11 +3,7 @@ import '../styles/Home.css';
 function Home() {
   return (
     <>
-      <div className='bg-container'>
-        <div>
-          <p className='title'>MADS</p>
-        </div>
-      </div>
+      <div className='bg-container'></div>
     </>
   )
 }
